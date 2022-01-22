d736559542d215d83efec1761d86d560ff2708bd
Thimovez <thimovez@gmail.com>
2022-01-22 23:49:06 +0300

    add new controller function

--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -1,22 +1,30 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
+import { ObjectId } from "mongoose";
+import { CreateTrackDto } from "./dto/create.track.dto";
+import { TrackService } from "./track.service";
 
 @Controller('/tracks')
 export class TrackController {
+    constructor(private trackService: TrackService) { }
     //создание трека
-    create() {
-
+    @Post()
+    create(@Body() dto: CreateTrackDto) {
+        return this.trackService.create(dto);
     }
+    //получить все треки
     @Get()
     getAll() {
-        return 'Work'
+        return this.trackService.getAll();
     }
     //доп информация о треке
-    getOne() {
-
+    @Get(':id')
+    getOne(@Param('id') id: ObjectId) {
+        return this.trackService.getOne(id);
     }
     //удалить трек
-    delete() {
-
+    @Delete(':id')
+    delete(@Param('id') id: ObjectId) {
+        return this.trackService.delete(id);
     }
 }
--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,7 +1,8 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
+import { Model, ObjectId } from "mongoose";
+import { CreateTrackDto } from "./dto/create.track.dto";
 import { Comment, CommentDocument } from "./shemas/comment.shema";
 import { Track, TrackDocument } from "./shemas/track.shema";
 
@@ -11,19 +12,23 @@ export class TrackService {
         @InjectModel(Comment.name) private commentModel: Model<CommentDocument>
     ) { }
     //создание трека
-    async create() {
-
+    async create(dto: CreateTrackDto): Promise<Track> {
+        const track = await this.trackModel.create({ ...dto, listens: 0 });
+        return track;
     }
     //получение всех треков из базы данных
-    async getAll() {
-
+    async getAll(): Promise<Track[]> {
+        const tracks = await this.trackModel.find();
+        return tracks;
     }
     //доп информация о треке
-    async getOne() {
-
+    async getOne(id: ObjectId): Promise<Track> {
+        const track = await this.trackModel.findById(id);
+        return track;
     }
     //удалить трек
-    async delete() {
-
+    async delete(id: ObjectId): Promise<ObjectId> {
+        const track = await this.trackModel.findByIdAndDelete(id);
+        return track._id;
     }
 }

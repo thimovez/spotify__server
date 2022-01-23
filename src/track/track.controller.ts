b4eafc210aaf463831414295bdccb66c3360f2b0
Thimovez <thimovez@gmail.com>
2022-01-23 23:32:50 +0300

    add new controller

--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -1,6 +1,8 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, UploadedFiles, UseInterceptors } from "@nestjs/common";
+import { FileFieldsInterceptor } from "@nestjs/platform-express";
 import { ObjectId } from "mongoose";
+import { CreateCommentDto } from "./dto/create.comment.dto";
 import { CreateTrackDto } from "./dto/create.track.dto";
 import { TrackService } from "./track.service";
 
@@ -9,8 +11,13 @@ export class TrackController {
     constructor(private trackService: TrackService) { }
     //создание трека
     @Post()
-    create(@Body() dto: CreateTrackDto) {
-        return this.trackService.create(dto);
+    @UseInterceptors(FileFieldsInterceptor([
+        { name: 'picture', maxCount: 1 },
+        { name: 'audio', maxCount: 1 },
+    ]))
+    create(@UploadedFiles() files, @Body() dto: CreateTrackDto) {
+        const { picture, audio } = files
+        return this.trackService.create(dto, picture[0], audio[0]);
     }
     //получить все треки
     @Get()
@@ -26,5 +33,10 @@ export class TrackController {
     @Delete(':id')
     delete(@Param('id') id: ObjectId) {
         return this.trackService.delete(id);
+    }
+
+    @Post('/comments')
+    addComment(@Body() dto: CreateCommentDto) {
+        return this.trackService.addComment(dto);
     }
 }
--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -2,6 +2,7 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model, ObjectId } from "mongoose";
+import { CreateCommentDto } from "./dto/create.comment.dto";
 import { CreateTrackDto } from "./dto/create.track.dto";
 import { Comment, CommentDocument } from "./shemas/comment.shema";
 import { Track, TrackDocument } from "./shemas/track.shema";
@@ -12,7 +13,7 @@ export class TrackService {
         @InjectModel(Comment.name) private commentModel: Model<CommentDocument>
     ) { }
     //создание трека
-    async create(dto: CreateTrackDto): Promise<Track> {
+    async create(dto: CreateTrackDto, picture, audio): Promise<Track> {
         const track = await this.trackModel.create({ ...dto, listens: 0 });
         return track;
     }
@@ -23,12 +24,20 @@ export class TrackService {
     }
     //доп информация о треке
     async getOne(id: ObjectId): Promise<Track> {
-        const track = await this.trackModel.findById(id);
+        const track = await this.trackModel.findById(id).populate('comments');
         return track;
     }
     //удалить трек
     async delete(id: ObjectId): Promise<ObjectId> {
         const track = await this.trackModel.findByIdAndDelete(id);
         return track._id;
+    }
+
+    async addComment(dto: CreateCommentDto): Promise<Comment> {
+        const track = await this.trackModel.findById(dto.trackId);
+        const comment = await this.commentModel.create({ ...dto });
+        track.comments.push(comment._id);
+        await track.save();
+        return comment;
     }
 }
